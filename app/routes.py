3d3a8109993760a0c5f1497e7fec2cc5777ab940
Malin Dulkies <kay.sian@gmx.de>
2019-05-02 13:06:43 +0200

    Erste Schritte zu Templates
    Template Ordner erstellt, 
    Hello World HTML-Doc erstellt,
    Routes dahingehend verändert

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
+from app import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('helloWorld.html')
