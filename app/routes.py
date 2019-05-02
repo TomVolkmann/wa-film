52887b085f485fb93eab987e424bc3e381fb8be7
Tom Volkmann <Tom.Volkmann@web.de>
2019-05-02 13:41:11 +0200

    First Database Input

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,21 @@
 from app import app
-from app import render_template
-
+from app import render_template, request, redirect
+import database
+ 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('helloWorld.html')
+    return render_template('helloWorld.html') 
+
+
+@app.route ('/add_entry', methods =['POST'])
+def add_entry () :
+    title = request.form ['title']
+    id = request.form ['id']
+    new_entry = {
+        'title': title,
+        'id' : id
+    }
+    print(new_entry["title"] + " Success")
+    database.saveNewMovie(id,title)
+    return redirect('/')
