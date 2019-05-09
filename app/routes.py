1dc8d00dd294cb16abfdd590839551cd45e59f50
Tom Volkmann <Tom.Volkmann@web.de>
2019-05-09 13:01:29 +0200

    Database updated

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
 from app import render_template, request, redirect
+import main
 import database
  
 @app.route('/')
@@ -10,14 +11,11 @@ def index():
 
 @app.route ('/add_entry', methods =['POST'])
 def add_entry () :
-    title = request.form ['title']
-    id = request.form ['id']
     new_entry = {
-        'title': title,
-        'id' : id
+        'title': request.form['title'],
+        'id' : request.form['id']
     }
-    print(new_entry["title"] + " Success")
-    database.saveNewMovie(id,title)
+    main.confirmInput(new_entry)
     return redirect('/')
 
 @app.route('/movies')
--- database.py
+++ database.py
@@ -1,16 +1,35 @@
-from sqlalchemy import Column, String, Integer, create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, String, Integer, create_engine, Table, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 ENGINE = create_engine('sqlite:///app_db.db')
 
+contact_movies= Table('contact_movies',
+    Base.metadata,
+    Column('contact_id',Integer ,ForeignKey ('contacts.id')),
+    Column('movie_id',Integer,ForeignKey('movies.id'))
+)
+
 class Movies(Base):
     __tablename__ = "movies"
 
-    movie_id = Column('movie_id', Integer, primary_key=True)
+    id = Column('id', Integer, primary_key=True)
     title = Column('title', String, unique=True)
-    #release_data = Column('releaseDate', String)
+    release_date = Column('releaseDate', String)
+    
+    #referring  to the secondary table
+    contacts = relationship("Contacts",secondary=contact_movies)
+
+class Contacts(Base):
+    __tablename__ = "contacts"
+
+    id = Column('id', Integer, primary_key = True)
+    name = Column('name', String)
+
+    #referring  to the secondary table
+    movies = relationship("Movies",secondary=contact_movies)
+
 
 Base.metadata.create_all(bind=ENGINE)
 Session = sessionmaker(bind=ENGINE)
