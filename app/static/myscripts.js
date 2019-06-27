9f69e6fd17f0fcb81868e06ad28c4176c72dd456
Malin Dulkies <kay.sian@gmx.de>
2019-06-27 19:18:52 +0200

    Link Highlighting

---
+++ app/static/myscripts.js
@@ -0,0 +1,21 @@
+function activeClass() {
+    var fullCurrentURL = window.location.href;
+    var CurrentURLparts = fullCurrentURL.split("/");
+    var CurrentURLindex = CurrentURLparts.length - 1;
+    var CurrentURL = CurrentURLparts[CurrentURLindex];
+    console.log(CurrentURL);
+  
+    $('.navbar-nav > li a').each(function() {
+      var fullLinkURL = $(this).attr('href');
+      var LinkURLparts = fullLinkURL.split("/");
+      var LinkURLindex = LinkURLparts.length - 1;
+      var LinkURL = LinkURLparts[LinkURLindex];
+      if (LinkURL === CurrentURL) {
+        $(this).parents("li").addClass("active");
+      }
+      if(LinkURL !== CurrentURL){
+          dashboard.addClass("active");
+      }
+    });
+  }
+  activeClass();
